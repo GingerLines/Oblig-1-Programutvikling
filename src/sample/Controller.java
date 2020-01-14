b3352b781c8b32b6f09adc3f7f4aea9e375b4680
Frida Lines <frida.lines@gmail.com>
2020-01-14 13:14:10 +0100

    Opprette filer
    Opprette filer

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField txtFødtDag;
+
+    @FXML
+    private TextField txtAlder;
+
+    @FXML
+    private TextField txtNavn;
+
+    @FXML
+    private TextField txtFødtMåned;
+
+    @FXML
+    private TextField txtFødtÅr;
+
+    @FXML
+    void sendSvar(ActionEvent event) {
+
+    }
+
+}
